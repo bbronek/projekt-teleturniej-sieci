318d2ca9497449e9c45e55f8daef046f85e8a5d8
Michał Gulczyński <s464953@labs.wmi.amu.edu.pl>
2022-01-21 21:15:16 +0100

    Add server-client

---
+++ client.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.TimeUnit;
+
+
+public class client {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+		Socket clientSocket = new Socket("localhost",3000);
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		outToServer.writeBytes("LOGIN 464953"+'\n');
+		while(true){
+		
+		}
+	}
+}
