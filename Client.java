9843989a11b9bc356e2e7f1a261809ddd20e4dbe
bbronek <barbro214@gmail.com>
2022-01-21 21:53:21 +0100

    Fix code style

--- Client.java
+++ Client.java
@@ -4,15 +4,15 @@ import java.net.*;
 import java.util.concurrent.TimeUnit;
 
 
-public class client {
+public class Client {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-		Socket clientSocket = new Socket("localhost",3000);
+		    Socket clientSocket = new Socket("localhost",3000);
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		outToServer.writeBytes("LOGIN 464953"+'\n');
-		while(true){
-		
-		}
+		    outToServer.writeBytes("LOGIN 464953"+'\n');
+        // while(true){
+        
+        // }
 	}
 }
---
+++ Server.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.TimeUnit;
+
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket listener = new ServerSocket(3000);
+        Socket socket = listener.accept();
+        BufferedReader inFromCleint = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+        
+        System.out.println("Tic Tac Toe Server is Running");
+        //zmiena przyjmuje input od clienta
+        //x=input
+        //x=2
+        //1[a,b,c]
+        //2[d,e,f]
+        System.out.println(inFromCleint.readLine());
+        Socket socket1 = listener.accept();
+        BufferedReader inFromCleint1 = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
+        DataOutputStream outToClient1 = new DataOutputStream(socket1.getOutputStream());
+        System.out.println(inFromCleint1.readLine());
+        // while(true){
+            
+        // }
+	}
+}
