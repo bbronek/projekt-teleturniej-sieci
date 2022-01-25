5500bdefbd655a2efb9066b37c19e2e24c72a7df
Michal Gulczynski <malygulczas@gmail.com>
2022-01-25 21:16:37 +0100

    Add ClientHandler

---
+++ src/com/company/ClientHandler.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+// ClientHandler class
+class ClientHandler implements Runnable
+{
+    Scanner scn = new Scanner(System.in);
+    private String name;
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    Socket s;
+    boolean isloggedin;
+
+    // constructor
+    public ClientHandler(Socket s, String name,
+                         DataInputStream dis, DataOutputStream dos) {
+        this.dis = dis;
+        this.dos = dos;
+        this.name = name;
+        this.s = s;
+        this.isloggedin=true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+
+        String received;
+        while (true)
+        {
+            try
+            {
+                // receive the string
+                received = dis.readUTF();
+
+                System.out.println(received);
+
+                if(received.equals("logout")){
+                    this.isloggedin=false;
+                    this.s.close();
+                    break;
+                }
+
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+        }
+        try
+        {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
