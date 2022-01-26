c72fd5357dc13be26ff26757eecef60ef8a86e12
bbronek <barbro214@gmail.com>
2022-01-26 04:32:33 +0100

    Add logic of setting questions

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -14,12 +14,45 @@ public class Server
     // counter for clients
     static int i = 0;
 
+    public static void setQuestions(List<Question> listOfQuestions) throws FileNotFoundException {
+        String line, answer = null;
+        String text = "";
+        String[] separated;
+        File questions = new File("src/com/company/questions.txt");
+        Scanner sc = new Scanner(questions);
+        Question question = new Question();
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            separated = line.split("\\=");
+
+            if (separated.length == 2) {
+                text += separated[0] + '\n';
+                answer = separated[1];
+            } else if(line == "") {
+                question.setText(text);
+                question.setAnswer(answer);
+                question.setId(i);
+                text = "";
+                answer = null;
+                listOfQuestions.add(question);
+                question = new Question();
+            } else {
+                text += separated[0] + '\n';
+            }
+        }
+
+        Collections.shuffle(listOfQuestions);
+    }
+
     public static void main(String[] args) throws IOException
     {
         // server is listening on port 1234
         ServerSocket ss = new ServerSocket(1234);
-
         Socket s;
+        List<Question> listOfQuestions = new ArrayList<>();
+
+        setQuestions(listOfQuestions);
 
         // running infinite loop for getting
         // client request
