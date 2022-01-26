a19b6e3af97f60845034328b3416f05231046739
bbronek <barbro214@gmail.com>
2022-01-26 04:31:06 +0100

    Create Question class

---
+++ src/com/company/Question.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Question {
+    int id;
+    String text, answer;
+
+    public void setId(int id) { this.id = id; }
+
+    public void setAnswer(String answer) { this.answer = answer; }
+
+    public void setText(String text) { this.text = text; }
+
+    public String getText() { return text; }
+
+    public String getAnswer() { return answer; }
+}
